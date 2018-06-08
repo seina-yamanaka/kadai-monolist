f2e21065951f52e3d9b7c14a3eab529a4567b2ed
seina.yamanaka <seina.a.yamanaka@rakuten.com>
2018-06-08 05:25:19 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -28,11 +28,23 @@ use \App\Item;
                 $items[] = $item;
             }
         }
+        
 
         return view('items.create', [
             'keyword' => $keyword,
             'items' => $items,
         ]);
+    }
+    
+     public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
     }
   }
   
